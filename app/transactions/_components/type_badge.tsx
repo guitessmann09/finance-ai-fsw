6df16c14bd3332ac0431c92260639f31769af70a
guitessmann09 <tessmanng9@gmail.com>
2024-11-06 22:18:32 -0300

    feat: add transaction type badge

---
+++ app/transactions/_components/type_badge.tsx
@@ -0,0 +1,33 @@
+import { Transaction, TransactionTypes } from "@prisma/client";
+import { Badge, CircleIcon } from "lucide-react";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionTypes.DEPOSIT) {
+    return (
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+        <CircleIcon className="mr-2 fill-primary" size={10} />
+        Depósito
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionTypes.EXPENSE) {
+    return (
+      <Badge className="bg-danger bg-opacity-10 font-bold text-danger hover:bg-danger hover:bg-opacity-10">
+        <CircleIcon className="mr-2 fill-danger" size={10} />
+        Despesa
+      </Badge>
+    );
+  }
+  return (
+    <Badge className="bg-muted font-bold text-white hover:bg-muted">
+      <CircleIcon className="mr-2 fill-white" size={10} />
+      Investimento
+    </Badge>
+  );
+};
+
+export default TransactionTypeBadge;
