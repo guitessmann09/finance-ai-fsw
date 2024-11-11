c907fcb5b1369d6a44eea7389d2f4fc611ab10b3
guitessmann09 <tessmanng9@gmail.com>
2024-11-10 23:48:17 -0300

    feat: add expenses per category component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,6 +6,7 @@ import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
 import TransactionPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "../_data/get-dashboard";
+import ExpensesPerCategory from "./_components/expenses-per-category";
 
 interface HomeProps {
   searchParams: {
@@ -32,11 +33,14 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <div className="grid grid-cols-[2fr,1fr]">
+        <div className="grid grid-cols-[2fr,1fr] gap-6">
           <div className="flex flex-col gap-6">
             <SummaryCards month={month} {...dashboard} />
-            <div className="gap 6 grid grid-cols-3 grid-rows-1">
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
               <TransactionPieChart {...dashboard} />
+              <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              />
             </div>
           </div>
         </div>
--- app/_data/get-dashboard/types.ts
+++ app/_data/get-dashboard/types.ts
@@ -1,5 +1,11 @@
-import { TransactionTypes } from "@prisma/client";
+import { TransactionCategory, TransactionTypes } from "@prisma/client";
 
 export type TransactionPercentagePerType = {
   [key in TransactionTypes]: number;
 };
+
+export interface TotalExpensePerCategory {
+  category: TransactionCategory;
+  totalAmount: number;
+  percentageOfTotal: number;
+}
