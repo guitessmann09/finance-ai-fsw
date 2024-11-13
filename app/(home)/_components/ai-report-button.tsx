9f3df4d524b2106fc73c3de97ff93ce50fe8e65d
guitessmann09 <tessmanng9@gmail.com>
2024-11-12 23:04:19 -0300

    feat: add ai report button

---
+++ app/(home)/_components/ai-report-button.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/_components/ui/dialog";
+import { BotIcon } from "lucide-react";
+
+const AiReportButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost">
+          Relatório IA <BotIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Relatório IA</DialogTitle>
+          <DialogDescription>
+            Use inteligência artificial para gerar um relatório com insights
+            sobre suas finanças.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="ghost">Cancelar</Button>
+          </DialogClose>
+          <Button>Gerar relatório</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AiReportButton;
