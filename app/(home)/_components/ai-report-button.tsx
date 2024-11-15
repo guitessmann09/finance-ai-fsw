bab6f829012d0780acd1a4bdc35669b9879c6f81
guitessmann09 <tessmanng9@gmail.com>
2024-11-14 22:25:56 -0300

    feat: add ai report generation

--- app/(home)/_components/ai-report-button.tsx
+++ app/(home)/_components/ai-report-button.tsx
@@ -11,17 +11,44 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/app/_components/ui/dialog";
-import { BotIcon } from "lucide-react";
+import { BotIcon, Loader2Icon } from "lucide-react";
+import { generateAiReport } from "../_actions/generate-ai-report";
+import { useState } from "react";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import Markdown from "react-markdown";
+interface AiReportButtonProps {
+  month: string;
+}
 
-const AiReportButton = () => {
+const AiReportButton = ({ month }: AiReportButtonProps) => {
+  const [report, setReport] = useState<string | null>(null);
+  const [reportIsLoading, setReportIsLoading] = useState(false);
+  const handleGenerateReportCLick = async () => {
+    try {
+      setReportIsLoading(true);
+      const aiReport = await generateAiReport({ month });
+      setReport(aiReport);
+      await generateAiReport({ month });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setReportIsLoading(false);
+    }
+  };
   return (
-    <Dialog>
+    <Dialog
+      onOpenChange={(open) => {
+        if (!open) {
+          setReport(null);
+        }
+      }}
+    >
       <DialogTrigger asChild>
         <Button variant="ghost">
           Relatório IA <BotIcon />
         </Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="max-w-[600px]">
         <DialogHeader>
           <DialogTitle>Relatório IA</DialogTitle>
           <DialogDescription>
@@ -29,11 +56,20 @@ const AiReportButton = () => {
             sobre suas finanças.
           </DialogDescription>
         </DialogHeader>
+        <ScrollArea className="prose max-h-[450px] text-white prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+          <Markdown>{report}</Markdown>
+        </ScrollArea>
         <DialogFooter>
           <DialogClose asChild>
             <Button variant="ghost">Cancelar</Button>
           </DialogClose>
-          <Button>Gerar relatório</Button>
+          <Button
+            onClick={handleGenerateReportCLick}
+            disabled={reportIsLoading}
+          >
+            {reportIsLoading && <Loader2Icon className="animate-spin" />}
+            Gerar relatório
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
